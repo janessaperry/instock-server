63032bbda099ebc57232a1943d8d5575aecb9b7c
janessaperry <hello@janessaperry.com>
2025-03-02 10:56:56 -0800

    set up database

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 import express from "express";
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
+const { SERVER_PORT, CORS_ORIGIN } = process.env;
 
-app.listen(8080, () => {
-  console.log("Server is running on port 8080");
+app.use(cors({ origin: CORS_ORIGIN }));
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(SERVER_PORT, () => {
+  console.log(`Server is running on port ${SERVER_PORT}`);
 });
