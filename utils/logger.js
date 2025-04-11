1228be10f8d5e5e31a8492d74358b2962f844a04
janessaperry <hello@janessaperry.com>
2025-04-11 16:28:28 -0700

    Add Winston logger for improved logging functionality

---
+++ utils/logger.js
@@ -0,0 +1,30 @@
+import winston from "winston";
+
+const logger = winston.createLogger({
+  level: "info",
+  format: winston.format.json(),
+  defaultMeta: { service: "instock-api" },
+  transports: [
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+      maxsize: 5242880,
+      maxFiles: 5,
+    }),
+    new winston.transports.File({
+      filename: "logs/combined.log",
+      maxsize: 5242880,
+      maxFiles: 5,
+    }),
+  ],
+});
+
+if (process.env.NODE_ENV !== "production") {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  );
+}
+
+export default logger;
