636a63c0a5cf0a79166b7702e11f163c6225cdb1
janessaperry <hello@janessaperry.com>
2025-04-27 16:34:53 -0700

    add trust proxy for express-rate-limit

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ import inventoriesRoutes from "./routes/inventories-routes.js";
 import "./scripts/cron/scheduled-seed.js";
 
 const app = express();
+app.set('trust proxy', true);
 const {SERVER_PORT, CORS_ORIGIN} = process.env;
 
 app.use(cors({origin: CORS_ORIGIN}));
